34a3ec93f681d0332b2e344c3a97266c78d12d7d
pranjal chaubey <pranjal.chaubey@mqube.ai>
2024-12-23 15:55:13 +0530

    intial proddfddssssafewq

--- src/Components/ApiConfig/BaseURL.jsx
+++ src/Components/ApiConfig/BaseURL.jsx
@@ -1,6 +1,7 @@
 // src/components/BaseURL.js
-const BaseURL = "http://18.117.9.226";
+// const BaseURL = "http://18.117.9.226";
 // const BaseURL = "http://127.0.0.1:8000";
+const BaseURL = "https://api.restnvest.com";
 
 
 

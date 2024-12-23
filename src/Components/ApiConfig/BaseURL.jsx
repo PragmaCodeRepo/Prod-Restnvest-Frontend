c4a531fd1a196ee84031f8f149e9a442eebd22c6
pranjal chaubey <pranjal.chaubey@mqube.ai>
2024-12-23 13:29:20 +0530

    intial proddfdds

--- src/Components/ApiConfig/BaseURL.jsx
+++ src/Components/ApiConfig/BaseURL.jsx
@@ -1,6 +1,6 @@
 // src/components/BaseURL.js
-// const BaseURL = "http://18.117.9.226";
-const BaseURL = "http://127.0.0.1:8000";
+const BaseURL = "http://18.117.9.226";
+// const BaseURL = "http://127.0.0.1:8000";
 
 
 export default BaseURL;
